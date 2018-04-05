b71b7279674040be64c1454c2876500e827e47cc
yeuchi@gmail.com <yeuchi@gmail.com>
2018-04-04 20:04:24 -0500

    exo-player

--- app/src/main/java/com/ctyeung/mybakingapp/StepDetailFragment.java
+++ app/src/main/java/com/ctyeung/mybakingapp/StepDetailFragment.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.VideoView;
@@ -16,10 +17,43 @@ import com.ctyeung.mybakingapp.data.Recipe;
 import com.ctyeung.mybakingapp.data.RecipeFactory;
 import com.ctyeung.mybakingapp.data.Step;
 import com.ctyeung.mybakingapp.utility.JSONHelper;
-
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.Timeline;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.util.Util;
 import org.json.JSONObject;
-
 import java.util.List;
+
+/////////////////////////////////
+
+import com.ctyeung.mybakingapp.StepDetailFragment;
+import com.google.android.exoplayer2.C;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.Timeline;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.upstream.TransferListener;
+import com.google.android.exoplayer2.util.Util;
+import android.content.Context;
 
 /**
  * Created by ctyeung on 3/17/18.
@@ -27,6 +61,22 @@ import java.util.List;
 
 public class StepDetailFragment extends BaseFragment
 {
+    // exo-player
+    private SimpleExoPlayerView simpleExoPlayerView;
+    private SimpleExoPlayer player;
+
+    private Timeline.Window window;
+    private DataSource.Factory mediaDataSourceFactory;
+    private DefaultTrackSelector trackSelector;
+    private boolean shouldAutoPlay;
+    private BandwidthMeter bandwidthMeter;
+
+    private ImageView ivHideControllerButton;
+    private Context context;
+    private Uri uri;
+
+    ////////////////////////////////////////////////////////////////
+
     private View rootView;
 
     private List<Step> mSteps;
@@ -39,9 +89,16 @@ public class StepDetailFragment extends BaseFragment
                              ViewGroup container,
                              Bundle saveInstanceState)
     {
+        context = this.getActivity();
+        shouldAutoPlay = true;
+        bandwidthMeter = new DefaultBandwidthMeter();
+        mediaDataSourceFactory = new DefaultDataSourceFactory(context, Util.getUserAgent(context, "mediaPlayerSample"), (TransferListener<? super DataSource>) bandwidthMeter);
+        window = new Timeline.Window();
 
         rootView = inflater.inflate(R.layout.fragment_step_detail, container, false);
         render();
+
+        //ivHideControllerButton = (ImageView) findViewById(R.id.exo_controller);
 
         return rootView;
     }
@@ -97,7 +154,9 @@ public class StepDetailFragment extends BaseFragment
     private void loadVideo(Step step)
     {
         TextView textView = (TextView)rootView.findViewById(R.id.fragment_video_nada);
-        VideoView videoView = (VideoView) rootView.findViewById(R.id.video_item);
+        //VideoView videoView = (VideoView) rootView.findViewById(R.id.video_item);
+        simpleExoPlayerView = (SimpleExoPlayerView) rootView.findViewById(R.id.player_view);
+
         ProgressBar progressBar = (ProgressBar)rootView.findViewById(R.id.fragment_progress);
         progressBar.setVisibility(View.INVISIBLE);
 
@@ -109,19 +168,101 @@ public class StepDetailFragment extends BaseFragment
         {
             // show error
             textView.setVisibility(View.VISIBLE);
-            videoView.setVisibility(View.INVISIBLE);
+            simpleExoPlayerView.setVisibility(View.INVISIBLE);
             return;
         }
         else {
-            Uri uri = (null==videoUri)?
+            uri = (null==videoUri)?
                         thumbnailUri:
                         videoUri;
 
             textView.setVisibility(View.INVISIBLE);
-            videoView.setVisibility(View.VISIBLE);
-            videoView.stopPlayback();
-            videoView.setVideoURI(uri);
-            videoView.start();
+            simpleExoPlayerView.setVisibility(View.VISIBLE);
+
+           // initializePlayer();
+
+            // videoView.stopPlayback();
+           // videoView.setVideoURI(uri);
+           // videoView.start();
+        }
+    }
+
+    //////////////////////////////////////////////
+    // exo-video-player
+
+
+    private void initializePlayer() {
+
+        simpleExoPlayerView.requestFocus();
+
+        TrackSelection.Factory videoTrackSelectionFactory =
+                new AdaptiveTrackSelection.Factory(bandwidthMeter);
+
+        trackSelector = new DefaultTrackSelector(videoTrackSelectionFactory);
+
+        player = ExoPlayerFactory.newSimpleInstance(context, trackSelector);
+
+        simpleExoPlayerView.setPlayer(player);
+
+        player.setPlayWhenReady(shouldAutoPlay);
+/*        MediaSource mediaSource = new HlsMediaSource(Uri.parse("https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8"),
+                mediaDataSourceFactory, mainHandler, null);*/
+
+        DefaultExtractorsFactory extractorsFactory = new DefaultExtractorsFactory();
+
+        //MediaSource mediaSource = new ExtractorMediaSource(Uri.parse("http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4"),
+        MediaSource mediaSource = new ExtractorMediaSource(Uri.parse(uri.toString()),
+        mediaDataSourceFactory, extractorsFactory, null, null);
+
+        player.stop();
+        player.prepare(mediaSource);
+/*
+        ivHideControllerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                simpleExoPlayerView.hideController();
+            }
+        }); */
+    }
+
+    private void releasePlayer() {
+        if (player != null) {
+            shouldAutoPlay = player.getPlayWhenReady();
+            player.release();
+            player = null;
+            trackSelector = null;
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (Util.SDK_INT > 23) {
+            initializePlayer();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if ((Util.SDK_INT <= 23 || player == null)) {
+            initializePlayer();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (Util.SDK_INT <= 23) {
+            releasePlayer();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (Util.SDK_INT > 23) {
+            releasePlayer();
         }
     }
 }
